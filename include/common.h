a404243bba9e44c497b4e6f28ba637424d6d57b0
xiaowhang <b13528723800@163.com>
2025-01-02 16:49:09 +0800

    ✨ 更新: 将常量宏定义替换为constexpr

--- include/common.h
+++ include/common.h
@@ -1,3 +1,6 @@
+#ifndef COMMON_H
+#define COMMON_H
+
 #include <iostream>
 #include <cstring>
 #include <sys/wait.h>
@@ -11,21 +14,23 @@
 #include <chrono>
 #include <thread>
 
-#define BASE_PORT 50000
-#define MAX_ROUTERS 100
+constexpr int MAX_ROUTERS = 100; // 最大路由器数量
 
-#define TIMEOUT_MILLISECONDS 1000
+constexpr int TIMEOUT_MILLISECONDS = 2000; // 超时时间
+constexpr int UPDATE_INTERVAL = 500;      // 周期更新间隔
 
-#define MSG_KEY_BASE 1000
-#define MANAGER_ID 999
+constexpr int MSG_KEY_BASE = 1000; // 消息队列键基础值
+constexpr int MANAGER_ID = 999;    // 管理器ID
 
-#define MSG_SIZE 1024
+constexpr int MSG_SIZE = 1024; // 消息数据大小
 
-#define MSG_TYPE_INIT 1
-#define MSG_TYPE_UPDATE 2
-#define MSG_TYPE_WAKE 3
-#define MSG_TYPE_TERMINATE 4
-#define MSG_TYPE_REFRESH 5
+constexpr int MSG_TYPE_INIT = 1;      // 消息类型：初始化
+constexpr int MSG_TYPE_UPDATE = 2;    // 消息类型：更新
+constexpr int MSG_TYPE_WAKE = 3;      // 消息类型：唤醒
+constexpr int MSG_TYPE_TERMINATE = 4; // 消息类型：终止
+constexpr int MSG_TYPE_REFRESH = 5;   // 消息类型：刷新
+
+constexpr int INF = 0x3f3f3f3f; // 无穷大
 
 // 定义消息结构
 struct Msg
@@ -34,3 +39,5 @@ struct Msg
     int src_id;    // 源ID
     char data[MSG_SIZE];
 };
+
+#endif // COMMON_H
